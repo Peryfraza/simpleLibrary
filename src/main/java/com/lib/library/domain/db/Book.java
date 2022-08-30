fd419016d191a4c6500de2958ecd14a668eb462b
alicj <alicja@olejnik.net>
2022-08-30 21:14:45 +0200

    Initial commit

---
+++ src/main/java/com/lib/library/domain/db/Book.java
@@ -0,0 +1,12 @@
+package com.lib.library.domain.db;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table
+public class Book {
+}
---
+++ src/main/java/com/lib/library/domain/db/User.java
@@ -0,0 +1,13 @@
+package com.lib.library.domain.db;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table
+public class User {
+
+}
